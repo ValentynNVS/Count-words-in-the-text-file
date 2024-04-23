e29e132c605a9fb1aa8bb0f716337bc7e5d727ed
ValentynNVS <novosidlak3@gmail.com>
2024-04-23 19:51:50 -0400

    Added a function that takeees of the newline charachter from the end of a string

--- Count words in the text file/source.cpp
+++ Count words in the text file/source.cpp
@@ -4,7 +4,7 @@
 
 
 const int kCharSize = 100;
-char takeOfLastFunction(char str[]);
+int takeOfLastFunction(char* str);
 
 int main(int argc, char* argv[]) {
 
@@ -17,15 +17,17 @@ int main(int argc, char* argv[]) {
 
 	if (argc >= 1) {
 		strcpy_s(filename, argv[1]);
+#pragma warning (disable : 4996);
 		filePointer = fopen(filename, "r"); // Open file in read mode
 		if (filePointer == NULL) {
-			printf("Error opening file.\n");
+			printf("Error opening file. \n");
 			return 1; // Return error code
 		}
 	}
 	
 	printf("Enter the word you would like to enter: \n");
 	fgets(wordName, kCharSize, stdin);
+	takeOfLastFunction(wordName);
 	//strcspn(wordName, '\n')
 	//while (!feof(filePointer)) {
 
@@ -34,7 +36,14 @@ int main(int argc, char* argv[]) {
 	//}
 }
 
-char takeOfLastFunction(char str[]) {
+int takeOfLastFunction(char* str) {
 
+
+	size_t len = strlen(str);
+	if ( str[len-1] == '\n') {
+		str[len - 1] = '\0';
+	}
+
+	return 0;
 
 }
