313318f2453fb576d14471049862c413ed5723a0
ValentynNVS <novosidlak3@gmail.com>
2024-04-23 14:45:18 -0400

    created a function prototype for the function which will take off the \n symbol that adds automaticlly by the fgets function

--- Count words in the text file/source.cpp
+++ Count words in the text file/source.cpp
@@ -4,18 +4,37 @@
 
 
 const int kCharSize = 100;
+char takeOfLastFunction(char str[]);
 
 int main(int argc, char* argv[]) {
 
 
 	char filename[kCharSize] = "";
-
+	char wordName[kCharSize] = "";
 	/*This part check if there is a=enough parameters to run the code*/
-	if (argc == 1) {
+
+	FILE* filePointer = NULL;
+
+	if (argc >= 1) {
 		strcpy_s(filename, argv[1]);
+		filePointer = fopen(filename, "r"); // Open file in read mode
+		if (filePointer == NULL) {
+			printf("Error opening file.\n");
+			return 1; // Return error code
+		}
 	}
-	else {
-		printf("Not enough parameters to run the program");
-		return 0;
-	}
+	
+	printf("Enter the word you would like to enter: \n");
+	fgets(wordName, kCharSize, stdin);
+	//strcspn(wordName, '\n')
+	//while (!feof(filePointer)) {
+
+
+
+	//}
+}
+
+char takeOfLastFunction(char str[]) {
+
+
 }
