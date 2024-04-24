fa04b29f190bd34b3bf8c316c5ff191be75cc551
ValentynNVS <novosidlak3@gmail.com>
2024-04-23 20:09:34 -0400

    Added a while loop which checks each word and counting how much of them are the same

--- Count words in the text file/source.cpp
+++ Count words in the text file/source.cpp
@@ -11,29 +11,54 @@ int main(int argc, char* argv[]) {
 
 	char filename[kCharSize] = "";
 	char wordName[kCharSize] = "";
-	/*This part check if there is a=enough parameters to run the code*/
+	char lineInfo[kCharSize] = "";
+	int wordCount = 0;
 
 	FILE* filePointer = NULL;
 
-	if (argc >= 1) {
+	/*This part check if there is enough parameters to run the code*/
+	if (argc == 1) {
 		strcpy_s(filename, argv[1]);
-#pragma warning (disable : 4996);
+	}
+	else {
+		printf("too many or not enough parameters");
+		return 0;
+	}
+	#pragma warning (disable : 4996);
 		filePointer = fopen(filename, "r"); // Open file in read mode
 		if (filePointer == NULL) {
 			printf("Error opening file. \n");
 			return 1; // Return error code
 		}
-	}
-	
+
 	printf("Enter the word you would like to enter: \n");
 	fgets(wordName, kCharSize, stdin);
 	takeOfLastFunction(wordName);
-	//strcspn(wordName, '\n')
-	//while (!feof(filePointer)) {
+	while (!feof(filePointer)) {
+		int i = 0;
+		int result = 0;
+		char empty[kCharSize] = "";
+		if (lineInfo[i] == '\n' || lineInfo[i] == '\t' || lineInfo[i] == '\0' || lineInfo[i] == ' ') {
+
+			result = strcmp(lineInfo, wordName);
+			if (result) {
+				wordCount++;
+			}
+			strcpy(lineInfo, empty);
+			continue;
+		}
+		else {
+			lineInfo[i] = fgetc(filePointer);
+			
+		}
+
+		
 
 
+		i++;
+	}
+	printf("The number of similar words is : %d\n", wordCount);
 
-	//}
 }
 
 int takeOfLastFunction(char* str) {
